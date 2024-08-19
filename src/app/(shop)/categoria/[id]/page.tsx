32e6be05159fa6c8ef1b0a2265d93509b9abd849
Jonathan Bautista <jonathan.bautista@arcsona.com>
2024-08-19 17:03:59 -0600

    dos

--- src/app/(shop)/categoria/[id]/page.tsx
+++ src/app/(shop)/categoria/[id]/page.tsx
@@ -1,47 +1,49 @@
 import { ProductGrid, Title } from '@/components';
-import { Category } from '@/interfaces';
+// import { Category } from '@/interfaces';
 import { initialData } from '@/seed/seed';
 import { notFound } from 'next/navigation';
 
 const seedProducts = initialData.products;
 
 
-interface Props {
-  params: {
-    id: Category;
-  }
-}
+// interface Props {
+//   params: {
+//     id: Category;
+//   }
+// }
 
 
-export default function Categoria({ params }: Props) {
+export default function Categoria() {
 
-  const { id } = params;
-  const products = seedProducts.filter( product => product.gender === id );
+  <h1>Hola</h1>
 
-  const labels: Record<Category, string>  = {
-    'men': 'para hombres',
-    'women': 'para mujeres',
-    'kid': 'para niños',
-    'unisex': 'para todos'
-  }
+  // const { id } = params;
+  // const products = seedProducts.filter( product => product.gender === id );
 
-  // if ( id === 'kids' ) {
-  //   notFound();
+  // const labels: Record<Category, string>  = {
+  //   'men': 'para hombres',
+  //   'women': 'para mujeres',
+  //   'kid': 'para niños',
+  //   'unisex': 'para todos'
   // }
 
+  // // if ( id === 'kids' ) {
+  // //   notFound();
+  // // }
 
-  return (
-    <>
-      <Title
-        title={`Artículos de ${ labels[id] }`}
-        subtitle="Todos los productos"
-        className="mb-2"
-      />
 
-      <ProductGrid 
-        products={ products }
-      />
+  // return (
+  //   <>
+  // //     <Title
+  // //       title={`Artículos de ${ labels[id] }`}
+  // //       subtitle="Todos los productos"
+  // //       className="mb-2"
+  // //     />
+
+  // //     <ProductGrid 
+  // //       products={ products }
+  // //     />
       
-    </>
-  );
+  //   </>
+  // );
 }
